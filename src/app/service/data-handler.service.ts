9bd6eceb981de12a62fa1e61887e27073776f43e
Igor Pavlenko <igorrrymbaaa@gmail.com>
2022-05-30 16:54:32 +0300

    add show tasks (without filter category)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../model/Category';
+import { Task } from '../model/Task';
 import { TestData } from '../data/TestData';
 
 @Injectable({
@@ -13,6 +14,10 @@ export class DataHandlerService {
 
   getCategories(): Category[] {
     return TestData.categories;
+  }
+
+  getTasks(): Task[] {
+    return TestData.tasks;
   }
 
 }
